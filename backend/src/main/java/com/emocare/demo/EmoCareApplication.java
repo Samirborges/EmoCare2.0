be52ef1d48b5e65d26dd80b473fe0f09c5900248
Samirborges <alessandro.samir@souunit.com.br>
2026-07-26 20:52:50 -0300

    feat(backend) Backend and dabase connection added

---
+++ backend/src/main/java/com/emocare/demo/EmoCareApplication.java
@@ -0,0 +1,13 @@
+package com.emocare.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmoCareApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/emocare/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emocare.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
