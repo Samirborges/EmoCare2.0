e04e73d88c3ed63b44f056b3ce60685107374ce9
Samirborges <alessandro.samir@souunit.com.br>
2026-08-02 12:54:24 -0300

    feat(UserResponseDTO): Adding field provider

--- backend/src/main/java/com/emocare/demo/DTO/UserResponseDTO.java
+++ backend/src/main/java/com/emocare/demo/DTO/UserResponseDTO.java
@@ -1,14 +1,21 @@
 package com.emocare.demo.DTO;
 
 import com.emocare.demo.entity.User;
+import com.emocare.demo.entity.enums.AuthProvider;
 import com.emocare.demo.entity.enums.UserRole;
 
 import java.util.UUID;
 
-public record UserResponseDTO(UUID id, String fullName, String email, UserRole role) {
+public record UserResponseDTO(
+        UUID id,
+        String fullName,
+        String email,
+        UserRole role,
+        AuthProvider provider
+) {
 
     public static UserResponseDTO from(User user) {
-        return new UserResponseDTO(user.getId(), user.getFullName(), user.getEmail(), user.getRole());
+        return new UserResponseDTO(user.getId(), user.getFullName(), user.getEmail(), user.getRole(), user.getProvider());
     }
 
 }
